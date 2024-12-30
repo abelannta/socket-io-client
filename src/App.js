59a639fe9320f8a1955afa29c7196c7e54d02a5b
Abel Ananta <61495043+abelannta@users.noreply.github.com>
2024-12-30 12:55:53 +0700

    improve: style chatbox

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 import { io } from "socket.io-client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCog, faTimes, faUser } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const socket = useRef();
   // State for storing messages and current message
   const [messages, setMessages] = useState([]);
   const [currentMessage, setCurrentMessage] = useState("");
+
   // Function to send a message
   const sendMessage = () => {
     if (currentMessage) {
@@ -47,35 +50,43 @@ function App() {
   return (
     <div className="App">
       <div className="chat-wrapper">
-        <div className="messages">
-          {messages.map((message, index) => (
-            <div
-              className={`chatbox ${
-                socket.current.id === message.user ? "current" : ""
-              }`}
-              key={index}
-            >
-              {socket.current.id !== message.user ? (
-                <div className="username">{message.user}</div>
-              ) : (
-                ""
-              )}
-              <div className="message">{message.message}</div>
-            </div>
-          ))}
-        </div>
-        <div className="input-wrapper">
-          <input
-            type="text"
-            placeholder="Type a message..."
-            value={currentMessage.message}
-            onChange={(e) => setCurrentMessage(e.target.value)}
-            required
-            className="input-message"
-          />
-          <button className="btn-send" onClick={sendMessage}>
-            Send
-          </button>
+        <div className="chatbox">
+          <div className="chatbox-header">
+            <div className="title">Realtime Chat - SocketIO</div>
+          </div>
+          <div className="chatbox-body">
+            {messages.map((message, index) => (
+              <div
+                className={`message ${
+                  socket.current.id === message.user ? "current-user" : ""
+                }`}
+                key={index}
+              >
+                <div className="avatar">
+                  <FontAwesomeIcon icon={faUser} />
+                </div>
+                <div className="content">
+                  {socket.current.id !== message.user ? (
+                    <div className="username">{message.user}</div>
+                  ) : (
+                    ""
+                  )}
+                  <div className="text">{message.message}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="chatbox-footer">
+            <input
+              type="text"
+              placeholder="Type a message..."
+              value={currentMessage.message}
+              onChange={(e) => setCurrentMessage(e.target.value)}
+              required
+            />
+            <button onClick={sendMessage}>Send</button>
+          </div>
         </div>
       </div>
     </div>
